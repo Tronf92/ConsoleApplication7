75df53d26db6a0e74e39f73e94ff64f69658a171
adrian <ady.niko51@gmail.com>
2017-03-07 13:31:51 +0200

    study

---
+++ ConsoleApplication7/ConsoleApplication7.cpp
@@ -0,0 +1,13 @@
+// ConsoleApplication7.cpp : main project file.
+
+#include "stdafx.h"
+#include "MyForm.h"
+
+using namespace System;
+
+int main(array<System::String ^> ^args)
+{
+	
+    Console::WriteLine(L"Hello World");
+    return 0;
+}
